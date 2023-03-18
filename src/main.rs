ac4684e87e4b49552130959ccdb9056c8789a55d
silasm01 <Silasmikkelsen@outlook.com>
2023-03-18 12:14:03 +0100

    Object system (Needs to be split up seperatly)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::{time::Duration, thread};
 
-use tui::{Tui, Text};
+use tui::{Tui, Text, Button, VerticalAlignment::*, HorizontalAlignment::*, PosTypes::*};
 
 mod tui;
 
 fn main() {
     let mut tui = Tui::new();
-    tui.add_obj(Text::new("Hello".to_string(), 5, 5));
+    tui.add_obj(tui::Object::Text(Text::new("Hello".to_string(), 5, 5)));
     tui.display();
     thread::sleep(Duration::from_millis(1000));
-    tui.add_obj(Text::new("Hello2".to_string(), 10, 10));
-    //tui.add_obj("hellot543".to_string());
+    tui.add_obj(tui::Object::Text(Text::new("Hello2".to_string(), 10, 10)));
+    tui.add_obj(tui::Object::Button(Button::new("hello3".to_string(), Left(Pixel(40)) , Top(Pixel(10)))));
     tui.display();
-    while true {
+    loop {
         
     }
 }
--- src/tui.rs
+++ src/tui.rs
@@ -2,30 +2,57 @@ use std::io::stdout;
 
 use crossterm::{
     execute,
-    cursor::MoveTo
+    cursor::MoveTo,
+    terminal::size,
 };
 
 #[derive(Debug)]
-pub struct Tui<T> {
-    objects: Vec<T>
+pub enum Object {
+    Text(Text),
+    Button(Button),
 }
 
-impl<U: std::fmt::Debug> Tui<U> {
-    pub fn new() -> Tui<U> {
-        Tui { objects: vec![] }
+#[derive(Debug)]
+pub struct Tui {
+    text: Vec<Text>,
+    button: Vec<Button>,
+}
+
+impl Tui {
+    pub fn new() -> Tui {
+        Tui { text: vec![], button: vec![] }
     }
 
-    pub fn add_obj(&mut self, object:U ) {
-        self.objects.append(&mut vec![object])
+    pub fn add_obj(&mut self, object: Object) {
+        match object {
+            crate::tui::Object::Text(t) => self.text.push(t),
+            crate::tui::Object::Button(b) => self.button.push(b),
+        }
     }
 
     pub fn display(&self) {
         print!("\x1B[2J\x1B[1;1H");
-        for obj in &self.objects {
-            println!("{:?}", obj)
-            //obj.display(&obj)
-        }
+        self.text.iter().for_each(|x| x.display(x));
+        self.button.iter().for_each(|x| x.display());
     }
+}
+
+#[derive(Debug)]
+pub enum PosTypes {
+    Pixel(i16),
+    Percent(i16),
+}
+
+#[derive(Debug)]
+pub enum VerticalAlignment {
+    Left(PosTypes),
+    Right(PosTypes)
+}
+
+#[derive(Debug)]
+pub enum HorizontalAlignment {
+    Top(PosTypes),
+    Bottom(PosTypes)
 }
 
 #[derive(Debug)]
@@ -35,13 +62,54 @@ pub struct Text {
     y: u16,
 }
 
+
 impl Text {
-    pub fn new(input: String, xin: u16, yin: u16) -> Text {
-        Text { string: input, x: xin, y: yin }
+    pub fn new(input: String, x: u16, y: u16) -> Text {
+        Text { string: input, x, y }
     }
 
     pub fn display(&self, obj: &Text) {
         execute!(stdout(), MoveTo(self.x, self.y)).unwrap();
-        println!("{:?}", obj.string)
+        println!("{}", obj.string)
+    }
+}
+
+#[derive(Debug)]
+pub struct Button {
+    string: String,
+    vertical_alignment: VerticalAlignment,
+    horizontal_alignment: HorizontalAlignment,
+}
+
+impl Button {
+    pub fn new(input: String, vertical_alignment: VerticalAlignment, horizontal_alignment: HorizontalAlignment) -> Button {
+        Button { string: input, vertical_alignment, horizontal_alignment }
+    }
+
+    pub fn display(&self) {
+        //println!("{}", obj.string)
+        let vertalignment = match &self.vertical_alignment {
+            VerticalAlignment::Left(u) => (u, 0),
+            VerticalAlignment::Right(u) => (u, size().unwrap().0 as i16*-1),
+        };
+
+        let verticalpostype: i16 = match vertalignment.0 {
+            PosTypes::Pixel(u) => (vertalignment.1+u).abs(),
+            PosTypes::Percent(u) => *u,
+        };
+
+        let horalignment = match &self.horizontal_alignment {
+            HorizontalAlignment::Top(u) => (u, 0),
+            HorizontalAlignment::Bottom(u) => (u, size().unwrap().1 as i16*-1),
+        };
+
+        let horticalpostype: i16 = match horalignment.0 {
+            PosTypes::Pixel(u) => (horalignment.1+u).abs(),
+            PosTypes::Percent(u) => *u,
+        };
+
+        execute!(stdout(), MoveTo(verticalpostype as u16, horticalpostype as u16)).unwrap();
+
+        println!("{}", self.string)
     }
 }
